68ed6cb1d0059687aa9a54e158a9c44e5f11674f
nobel <nobel2073@gmail.com>
2025-03-19 17:13:54 +0545

    chore: remove default config value

--- src/config.rs
+++ src/config.rs
@@ -13,17 +13,17 @@ pub struct Config {
 impl Config {
     pub fn from_env() -> Result<Self> {
         let database_url =
-            env::var("DATABASE_URL").context("Failed to read DATABASE_URL environment variable")?;
+            env::var("DATABASE_URL").context("Missing DATABASE_URL environment variable")?;
         let evm_rpc_url =
-            env::var("EVM_RPC_URL").unwrap_or_else(|_| "ws://localhost:8546".to_owned());
+            env::var("EVM_RPC_URL").context("Missing EVM_RPC_URL environment variable")?;
         let svm_rpc_url =
-            env::var("SVM_RPC_URL").unwrap_or_else(|_| "http://127.0.0.1:8899".to_owned());
+            env::var("SOLANA_RPC_URL").context("Missing SOLANA_RPC_URL environment variable")?;
         let twine_rpc_url =
-            env::var("TWINE_RPC_URL").unwrap_or_else(|_| "ws://127.0.0.1:8546".to_owned());
+            env::var("TWINE_RPC_URL").context("Missing TWINE_RPC_URL environment variable")?;
         let api_port = env::var("HTTP_PORT")
-            .map(|addr| addr.parse().context("Invalid HTTP_PORT format"))
-            .unwrap_or_else(|_| Ok(7777))
-            .context("Failed to parse HTTP_PORT")?;
+            .context("Missing HTTP_PORT environment variable")?
+            .parse()
+            .context("Invalid HTTP_PORT format")?;
 
         Ok(Self {
             database_url,
